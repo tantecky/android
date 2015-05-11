97f3424e483bcbad5303643c8637d2f3fe86f478
tantecky <tantecky@seznam.cz>
2015-05-11 12:31:36 +0200

    work on parser, merge later

--- OfflineDPP/app/src/main/java/com/tantecky/offlinedpp/net/ArrivalsFetcher.java
+++ OfflineDPP/app/src/main/java/com/tantecky/offlinedpp/net/ArrivalsFetcher.java
@@ -3,7 +3,6 @@ package com.tantecky.offlinedpp.net;
 
 import com.tantecky.offlinedpp.Utils;
 import com.tantecky.offlinedpp.model.Arrival;
-import com.tantecky.offlinedpp.model.DayType;
 import com.tantecky.offlinedpp.model.Line;
 
 import org.jsoup.Jsoup;
@@ -18,6 +17,8 @@ import java.util.GregorianCalendar;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class ArrivalsFetcher {
     //region static members
@@ -54,7 +55,6 @@ public final class ArrivalsFetcher {
     }
 
     private static void addArrivals(Element table, List<Arrival> arrivals) throws ParserException {
-        DayType dayType = getDayType(table);
 
         Elements rows = table.select("tbody tr");
 
@@ -82,23 +82,105 @@ public final class ArrivalsFetcher {
 
             for (Element minuteElement : minuteElements) {
                 // TODO new method addEntry
+
+                boolean lowFloor = minuteElement.toString().contains("nízkopodlažním");
+
+                // TODO make private final static
+                Pattern minutePattern = Pattern.compile("(\\d+)([a-zA-Z]*)");
+                Matcher minuteMatcher = minutePattern.matcher(minuteElement.text());
+
+                if (!minuteMatcher.find())
+                    throw new ParserException("Unable to find minute match: "
+                            + minuteElement.toString());
+
+                String minuteMatch = minuteMatcher.group(1);
+
+                if (Utils.isNullOrEmpty(minuteMatch))
+                    throw new ParserException("Unable to find minute match group: "
+                            + minuteElement.toString());
+
+                String noteMatch = minuteMatcher.group(2);
+
+                // we do not have a note
+                if (Utils.isNullOrEmpty(noteMatch))
+                    noteMatch = null;
+
+                int minute;
+                try {
+                    minute = Integer.parseInt(minuteMatch);
+                } catch (NumberFormatException ex) {
+                    throw new ParserException("Unable to convert minute to int: " + minuteMatch);
+                }
+
+                if (minute < 0 || minute > 59)
+                    throw new ParserException("Invalid minute value: " + Integer.toString(minute));
+
+                // TODO make private final static
+                Pattern intervalPattern = Pattern.compile("(\\d+)[-]?(\\d*)");
+
+                // TODO to a new method
+                if (minuteElement.text().contains("int.")) {
+                    Matcher intervalMatcher = intervalPattern.matcher(minuteElement.text().substring(minuteMatcher.end()));
+                    if (!intervalMatcher.find())
+                        throw new ParserException("Unable to find interval match: "
+                                + minuteElement.toString());
+
+                    String startIntervalMatch = intervalMatcher.group(1);
+
+                    if (Utils.isNullOrEmpty(startIntervalMatch))
+                        throw new ParserException("Unable to find start interval match group: "
+                                + minuteElement.toString());
+
+                    int startInterval = 0;
+                    try {
+                        startInterval = Integer.parseInt(startIntervalMatch);
+
+                        if (startInterval < 1)
+                            throw new ParserException("Start interval is less than 1 minute: "
+                                    + Integer.toString(startInterval));
+
+                    } catch (NumberFormatException ex) {
+                        throw new ParserException("Unable to convert start interval to int: " + startIntervalMatch);
+                    }
+
+                    String endIntervalMatch = intervalMatcher.group(2);
+                    int endInterval = 0;
+
+                    // we have an end interval
+                    if (!Utils.isNullOrEmpty(endIntervalMatch)) {
+                        try {
+                            endInterval = Integer.parseInt(endIntervalMatch);
+
+                            if (endInterval < 1)
+                                throw new ParserException("End interval is less than 1 minute: "
+                                        + Integer.toString(endInterval));
+
+                            if (startInterval >= endInterval)
+                                throw new ParserException(
+                                        String.format("Start interval (%d) >= end interval (%):"
+                                                , startInterval, endInterval));
+
+                        } catch (NumberFormatException ex) {
+                            throw new ParserException("Unable to convert end interval to int: " + endIntervalMatch);
+                        }
+                    }
+
+                    double delta = endInterval == 0 ?
+                            startInterval : (startInterval + endInterval) / 2.0;
+
+                    for (double newMinute = minute; newMinute < 60.0; newMinute += delta) {
+                        // add it into a list
+                        Utils.log(String.format("%d:%d", hour, (int)Math.floor(newMinute)));
+                    }
+                }
+                else
+                {
+                    // no interval, just add it into a list
+                    Utils.log(String.format("%d:%d", hour, minute));
+                }
             }
         }
 
-    }
-
-    private static DayType getDayType(Element table) throws ParserException {
-        Elements element = table.select("thead th:eq(1)");
-
-        if (element.size() != 1)
-            throw new ParserException("Missing table header");
-
-        try {
-            DayType dayType = DayType.fromString(element.text());
-            return dayType;
-        } catch (IllegalArgumentException ex) {
-            throw new ParserException(ex.getMessage());
-        }
     }
 
     private static String fetchArrivals(HttpClient client, Line line) throws IOException {
