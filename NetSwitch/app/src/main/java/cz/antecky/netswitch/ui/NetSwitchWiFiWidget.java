69007f3b91ec6d757ac31ef3078f765865bafeb0
Tomas Antecky <tomas@antecky.cz>
2018-02-17 16:27:31 +0100

    NetSwitch: WiFi does not require extra permissions

--- NetSwitch/app/src/main/java/cz/antecky/netswitch/ui/NetSwitchWiFiWidget.java
+++ NetSwitch/app/src/main/java/cz/antecky/netswitch/ui/NetSwitchWiFiWidget.java
@@ -1,9 +1,17 @@
 package cz.antecky.netswitch.ui;
 
+import android.content.Context;
+
 public class NetSwitchWiFiWidget extends NetSwitchWidget {
     @Override
     protected boolean ShowMobileButton() {
         return false;
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        // No dangerous/root permissions required
     }
 }
 
