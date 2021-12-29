7841c83625af771fbdaba4515a85accc4185eefe
Tomas Antecky <antecky@numsolution.cz>
2021-12-29 18:00:21 +0100

    FluidX: Improve splash effect

--- FluidX/app/src/main/res/raw/touch_fs.glsl
+++ FluidX/app/src/main/res/raw/touch_fs.glsl
@@ -2,9 +2,8 @@
 precision mediump float;
 #endif
 
-// increase SCALE to mark a neighbour texel
-// 0.5 is one texel
-#define SCALE 1.5
+#define FACTOR 3.0
+#define RADIUS 0.1
 
 varying vec2 v_texCoord;
 
@@ -16,17 +15,18 @@ uniform float u_time;
 uniform sampler2D u_temperature;
 
 void main(){
+    float x = v_texCoord.x;
+    float y = v_texCoord.y;
+
+    float splash = smoothstep(u_touch.x - RADIUS, u_touch.x + RADIUS, x)
+                 * smoothstep(u_touch.x + RADIUS, u_touch.x - RADIUS, x)
+                 * smoothstep(u_touch.y - RADIUS, u_touch.y + RADIUS, y)
+                 * smoothstep(u_touch.y + RADIUS, u_touch.y - RADIUS, y);
+    splash *= FACTOR;
+
     float temperature = texture2D(u_temperature, v_texCoord).x;
+    temperature = clamp(temperature + splash, 0.0f, 1.0f);
 
-    float xCenter = v_texCoord.x;
-    float yCenter = v_texCoord.y;
-
-    float dx = abs(xCenter - u_touch.x);
-    float dy = abs(yCenter - u_touch.y);
-
-    if (dx < SCALE * u_widthTexel && dy < SCALE * u_heightTexel) {
-        temperature = 1.0f;
-    }
 
     gl_FragColor = vec4(temperature, 0.0, 0.0, 1.0);
 }
