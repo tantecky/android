6913f4689b532c9439c4b28a9875deca2b5568ea
Tomas Antecky <antecky@numsolution.cz>
2021-12-30 13:59:56 +0100

    FluidX: Adjust onTouchEvent

--- FluidX/app/src/main/java/cz/antecky/fluidx/MyGLSurfaceView.kt
+++ FluidX/app/src/main/java/cz/antecky/fluidx/MyGLSurfaceView.kt
@@ -27,22 +27,21 @@ class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
 
             when (it.action) {
                 MotionEvent.ACTION_DOWN -> {
-                  //  Log.d(this::class.qualifiedName, "onTouchEvent: ACTION_DOWN x:$x y:$y")
+                    // Log.d(this::class.qualifiedName, "onTouchEvent: ACTION_DOWN x:$x y:$y")
                     return true
                 }
                 MotionEvent.ACTION_MOVE -> {
                     val s = x / this.width
                     val t = (this.height - y) / this.height
-                   /* Log.d(
-                        this::class.qualifiedName,
-                        "onTouchEvent: ACTION_MOVE x:$x y:$y s:$s t:$t"
-                    )*/
+                    /* Log.d(
+                         this::class.qualifiedName,
+                         "onTouchEvent: ACTION_MOVE x:$x y:$y s:$s t:$t"
+                     )*/
                     this.queueEvent { renderer.onTouch(s, t) }
 
                 }
                 MotionEvent.ACTION_UP -> {
                     //Log.d(this::class.qualifiedName, "onTouchEvent: ACTION_UP x:$x y:$y")
-                    this.queueEvent { renderer.onTouch(-1.0f, -1.0f) }
                 }
                 else -> {}
             }
